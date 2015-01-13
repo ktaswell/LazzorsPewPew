dc806916cb426ad86c9e7aea4a0b89c357109784
VersionInfin1te <asigal55@gmail.com>
2015-01-13 11:08:20 -0800

    Multiple Photoresisters - P: A: 1
    Preliminary software for multiple recivers.

---
+++ Programs/LazierMulti/LazierMulti.ino
@@ -0,0 +1,36 @@
+#include <Servo.h> 
+int line1Pin = 0; //analog pin 0
+int line2Pin = 1;
+int line3Pin = 2;
+
+boolean on[3];
+Servo myServo;  
+int pos = 0;
+
+void setup(){
+  Serial.begin(9600);
+  myServo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop(){
+  detect(0);
+  detect(1);
+  act();
+}
+
+void detect(int i) {
+    int lightVal = analogRead(i);
+    if (lightVal > 300) {
+      on[i] = true;
+    } else {
+      on[i] = false;
+    }
+}
+
+void act() {
+  if (on[1] == true) {
+    myServo.write(90);
+  } else {
+    myServo.write(0);
+  }
+}
